bae6e0d7395a10d3097e2f91299f5c60c222d891
hamzamak <yahyahamza50@gmail.com>
2024-09-22 13:35:52 +0100

    final commit

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import './Main.css'
 import { assets } from '../../assets/assets';
+import { Context } from '../../context/Context';
 
 function Main() {
+    const { onSent, recentPrompt, showResult, loading, resultData, setInput, input } = useContext(Context);
+
     return (
         <div className="main">
             <div className="nav">
@@ -11,48 +14,77 @@ function Main() {
             </div>
 
             <div className="main-container">
-                <div className="greet">
-                    <p><span>Hello, Dev.</span></p>
-                    <p>How can I help you today?</p>
+
+                {
+                    !showResult ?
+                        <>
+                            <div className="greet">
+                                <p><span>Hello, Dev.</span></p>
+                                <p>How can I help you today?</p>
+                            </div>
+                            <div className="cards">
+                                <div className="card">
+                                    <p>Suggest beautiful places to see on an upcoming road trip</p>
+                                    <img src={assets.compass_icon} alt="" />
+                                </div>
+
+                                <div className="card">
+                                    <p>Briefly summarize this concept: urban planning</p>
+                                    <img src={assets.bulb_icon} alt="" />
+                                </div>
+                                <div className="card">
+                                    <p>Brainstorm team bonding activities for our work retreat</p>
+                                    <img src={assets.message_icon} alt="" />
+                                </div>
+                                <div className="card">
+                                    <p>Improve the readability of the following code</p>
+                                    <img src={assets.code_icon} alt="" />
+                                </div>
+                            </div>
+                        </>
+                        :
+                        <div className="result">
+                            <div className="result-title">
+                                <img src={assets.user_icon} alt="" />
+                                <p>{recentPrompt}</p>
+                            </div>
+
+                            <div className="result-data">
+                                <img src={assets.gemini_icon} alt="" />
+                                {
+                                    loading ?
+                                        <div className='loader'>
+                                        </div>
+                                        :
+                                        <p dangerouslySetInnerHTML={{ __html: resultData }}>{resultData}</p>
+
+                                }
+                            </div>
+
+                        </div>
+
+                }
+
+
+
+
+
+
+                <div className="main-bottom">
+                    <div className="search-box">
+                        <input value={input} onChange={(e) => setInput(e.target.value)} type="text" placeholder='Enter a prompt here' />
+                        <div>
+                            <img src={assets.gallery_icon} alt='' />
+                            <img src={assets.mic_icon} alt='' />
+                           {input ?  <img onClick={() => onSent()} src={assets.send_icon} alt='' /> : null}
+                        </div>
+                    </div>
+
+                    <p className="bottom-info">
+                        this IA may display inaccurate info
+                    </p>
                 </div>
-                <div className="cards">
-                    <div className="card">
-                        <p>Suggest beautiful places to see on an upcoming road trip</p>
-                        <img src={assets.compass_icon} alt="" />
-                    </div>
-
-                    <div className="card">
-                        <p>Briefly summarize this concept: urban planning</p>
-                        <img src={assets.bulb_icon} alt="" />
-                    </div>
-                    <div className="card">
-                        <p>Brainstorm team bonding activities for our work retreat</p>
-                        <img src={assets.message_icon} alt="" />
-                    </div>
-                    <div className="card">
-                        <p>Improve the readability of the following code</p>
-                        <img src={assets.code_icon} alt="" />
-                    </div>
-                </div>
-
-           
-
-
-            <div className="main-bottom">
-                <div className="search-box">
-                    <input type="text" placeholder='Enter a prompt here' />
-                    <div>
-                        <img src={assets.gallery_icon} alt='' />
-                        <img src={assets.mic_icon} alt='' />
-                        <img src={assets.send_icon} alt='' />
-                    </div>
-                </div>
-
-                <p className="bottom-info">
-                    this IA may display inaccurate info
-                </p>
             </div>
-        </div>
         </div>
     );
 };
--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -4,20 +4,56 @@ import runChat from "../config/gemini";
 export const Context = createContext();
 
 const ContextProvider = (props) => {
-   
-   const [input , setInput] = useState("");
-   const [recentPrompt , setRecentPrompt] = useState([]);
-   const [prevPrompts,setPrevPrompts]= useState(false);
-   const [showResult , setShowResult] = useState(false);
-   const [loading , setLoading] = useState(false);
-   const [resultData , setResultData] = useState("");
 
-   const onSent = async (prompt) =>{
-      setResultData("")
-      setLoading(true)
-      setShowResult(true)
-      await runChat(input)
-   }
+    const [input, setInput] = useState("");
+    const [recentPrompt, setRecentPrompt] = useState([]);
+    const [prevPrompts, setPrevPrompts] = useState(false);
+    const [showResult, setShowResult] = useState(false);
+    const [loading, setLoading] = useState(false);
+    const [resultData, setResultData] = useState("");
+
+    const delayPara = (index, nextWord) => {
+        setTimeout(function(){
+            setResultData(prev => prev+nextWord)
+        } , 75 *index)
+    }
+
+    const newChat = ()=> {
+        setLoading(false);
+        setShowResult(false);
+    }
+
+    const onSent = async (prompt) => {
+        setResultData("")
+        setLoading(true)
+        setShowResult(true)
+        let response;
+
+        if (prompt !== undefined) {
+            response = await runChat(prompt)
+            setRecentPrompt(prompt)
+
+        }
+        else {
+            setPrevPrompts(prev => [...prev, input])
+            setRecentPrompt(input)
+            response = await runChat(input)
+        }
+
+        let responseArray = response.split("**")
+        let newResponse = "";
+        for (let i = 0; i < responseArray.length; i++) {
+            if (i === 0 || i % 2 !== 1) {
+                newResponse += responseArray[i];
+            } else {
+                newResponse += "<b>" + responseArray[i] + "</b>";
+            }
+        }
+
+        setResultData(newResponse)
+        setLoading(false)
+        setInput("")
+    }
 
 
 
@@ -31,11 +67,12 @@ const ContextProvider = (props) => {
         loading,
         resultData,
         input,
-        setInput
+        setInput,
+        newChat
 
     }
 
-    return(
+    return (
         <Context.Provider value={contextValue}>
             {props.childern}
         </Context.Provider>
