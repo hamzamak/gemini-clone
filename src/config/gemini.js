ec2eb99db6905f7a23c5f873bd317caa014ca817
hamzamak <yahyahamza50@gmail.com>
2024-09-22 14:00:41 +0100

    to add gemini config

---
+++ src/config/gemini.js
@@ -0,0 +1,9 @@
+// const { GoogleGenerativeAI } = require("@google/generative-ai");
+// const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+
+// async function runChat() {
+//   const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash"});
+//   const result = await model.generateContent(["Explain how AI works"]);
+//   console.log(result.response.text());
+// }
+// runChat();
